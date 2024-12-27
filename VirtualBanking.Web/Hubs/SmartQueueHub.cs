f999a4f306de1c0b515008f65bdc375b9944bbd8
Jacsel Guridi <jguridi@intelutions.net>
2024-12-26 20:05:09 -0400

    first commit

---
+++ VirtualBanking.Web/Hubs/SmartQueueHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace VirtualBanking.Web.Hubs
+{
+    public class SmartQueueHub : Hub
+    {
+        public async Task SendQueue(string message)
+        {
+            await Clients.All.SendAsync("ReceiveQueue", message);
+        }
+    }
+}
---
+++ VirtualBanking.Web/Program.cs
@@ -0,0 +1,25 @@
+using VirtualBanking.Web.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapHub<SmartQueueHub>("/smartqueue");
+
+app.MapControllers();
+
+app.Run();
